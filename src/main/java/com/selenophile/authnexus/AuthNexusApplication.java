24f94ba2d49d339ca0a8362a4a6b3d9ec1e39d1c
Kunal Goswami <kunalgoswami99@gmail.com>
2026-03-28 23:35:02 +0530

    First Commit || Running on Port 8080 || Auth Nexus

---
+++ src/main/java/com/selenophile/authnexus/AuthNexusApplication.java
@@ -0,0 +1,13 @@
+package com.selenophile.authnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selenophile/authnexus/AuthNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selenophile.authnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
